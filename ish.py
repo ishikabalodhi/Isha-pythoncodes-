27e77f762069ce8674a024f42badd2501881a2e9
ishikabalodhi <156896200+ishikabalodhi@users.noreply.github.com>
2024-01-27 09:41:27 +0530

    Add files via upload

---
+++ ish.py
@@ -0,0 +1,12 @@
+print("bhalla is innocent,true")
+
+#write a program to start stop or quite a car
+car=input("enter the sambhavnaein")
+while car != "stop":
+    print("danger")
+if car== "start":
+    print("car shuru ho rhi hai")
+elif car== "stop":
+    print("car ruk gyi")
+else:
+    print("vishram")
